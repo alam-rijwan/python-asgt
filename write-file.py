f096b75e2e0f827515ce5a8f744d37c743562cff
Rijwan <rijwanalam083@gmail.com>
2026-01-08 10:40:06 +0000

    write somthing on sample.txt

---
+++ write-file.py
@@ -0,0 +1,7 @@
+# Write to File program
+file = open("sample.txt", "w")
+file.write("Hello, This is python file handling example of sample.txt file.\n")
+file.write("This file is created using write mode.\n")
+file.close()
+
+print("Data written to file successfully.")
