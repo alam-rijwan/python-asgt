10827e90811e1b46e15c571e83c458eddd11f6d5
Rijwan <rijwanalam083@gmail.com>
2026-01-08 09:17:58 +0000

    initial commit

---
+++ grade_chacker.py
@@ -0,0 +1,12 @@
+# Grade Checker program
+score = int(input("Please Enter Your Score: "))
+if score >= 90:
+    print("Your Grade is A")
+elif score >= 80:
+    print("Your Grade is B")
+elif score >= 70:
+    print("Your Grade is C")
+elif score >= 60:
+    print("Your Grade is D")
+else:
+    print("Your Grade is F")
